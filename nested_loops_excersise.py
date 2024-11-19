73aec3298a318afbf1bd62a0c3b6aec0f583da26
Denis <ndenis@cps.k12.il.us>
2024-11-19 12:05:22 -0600

    worked on CRUD

---
+++ nested_loops_excersise.py
@@ -0,0 +1,80 @@
+# import student_data
+
+
+# students=student_data.students
+# # Number 1
+
+# # for students in students:
+# #     print(students['combo,email'])
+# #     print(students['Email'][0])
+# #     print(students['Email'][1])
+    
+
+
+
+# # Number 2
+    
+# H_r = input("Enter a homeroom number here: ")
+
+# # Flag to track if any student is found
+# found = False
+
+# for student in students:
+#     if H_r == student['HR']:
+#         print(student['Combo,Name'])
+#         print(student['Email'][0])
+#         print(student['Email'][1])
+#         print("_" * 25)
+#         found = True  # Set flag to True if a student is found
+
+# # Check the flag after the loop to print the message only once if no student is found
+# if not found:
+#     print("No one found in this homeroom")
+
+
+
+
+        
+
+
+
+
+
+
+
+# # Number 3
+
+# # input('What is your name? ')
+# #     if  input == Students
+# #      print(students[HR])
+# #      print(students['Combo,Name'])
+# #       
+
+
+
+
+# # # Number 4
+# G_l= int(input('enter a grade level?'))
+# for students in students:
+#     if G_l== students['GL']:
+    
+   
+   
+   
+   
+   
+# #  print(students[grade=10])
+# #     print(students['Combo,Name'])
+# #     
+# # number 5 
+# # for students in students 
+
+
+
+
+
+    
+
+
+
+
